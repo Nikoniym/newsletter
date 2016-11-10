61f5ae741ceb0871a03080afd24957da4130933b
Nickolay Korotaev <worldnillin@gmail.com>
2016-11-10 13:42:46 +0300

    ckedit work

--- app/admin/customer.rb
+++ app/admin/customer.rb
@@ -1,13 +1,15 @@
 ActiveAdmin.register Customer do
 
   index do
-    column 'Avatar' do |ad|
-      image_tag(ad.avatar(:thumb))
+    column 'Avatar' do |user|
+      image_tag(user.avatar(:thumb))
     end
     column :name
-    column :description
+    column 'description' do |user|
+      user.description.html_safe
+    end
     column :position_id
-
+    javascript_include_tag Ckeditor.cdn_url
     actions
   end
   permit_params :avatar, :name, :description, :position_id, :avatar_crop_x, :avatar_crop_y, :avatar_crop_w, :avatar_crop_h
@@ -22,7 +24,11 @@ ActiveAdmin.register Customer do
       f.label :name
       f.text_field :name
       f.label :description
-      f.text_area :description
+      # f.text_area :description
+      # f.cktext_area :description, class: 'someclass', ckeditor: { language: 'uk'}
+      # f.cktext_area :description, value: 'Default value', id: 'sometext'
+      f.cktext_area :description,  cols: 40, ckeditor: { uiColor: '#E6E8E8', toolbar: 'mini' }
+
       f.label :position_id
       f.collection_select :position_id, Position.all, :id, :name
 
@@ -30,11 +36,16 @@ ActiveAdmin.register Customer do
     f.actions
   end
 
-  show do |ad|
+  show do |user|
     attributes_table do
       row :avatar do
-        image_tag(ad.avatar (:thumb))
+        image_tag(user.avatar (:thumb))
       end
+      row  :name
+      row 'description' do |user|
+        user.description.html_safe
+      end
+      row  :position_id
       # Will display the image on show object page
     end
   end
--- app/assets/javascripts/active_admin.js.coffee
+++ app/assets/javascripts/active_admin.js.coffee
@@ -1,3 +1,4 @@
 #= require active_admin/base
 #= require jquery.jcrop
 #= require papercrop
+#= require ckeditor/init
