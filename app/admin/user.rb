cb2c55b4669d146d31031485bbc9dc0acacac90c
Nickolay Korotaev <worldnillin@gmail.com>
2016-11-10 17:16:03 +0300

    ckeditor work

---
+++ app/admin/user.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register User do
+
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+  permit_params :avatar, :name, :description, :position_id, :avatar_crop_x, :avatar_crop_y, :avatar_crop_w, :avatar_crop_h, :email
+
+end
